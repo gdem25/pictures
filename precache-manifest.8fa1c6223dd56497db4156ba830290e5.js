eec6e5c180228ce0cd716593ad5ba1820ba35a91
unknown <gar.dem494@gmail.com>
2020-03-23 02:56:31 -0700

    Updates

---
+++ precache-manifest.8fa1c6223dd56497db4156ba830290e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4363cf44c27a64b4b2ee36d2ddec436a",
+    "url": "/pictures/index.html"
+  },
+  {
+    "revision": "faef04adf4487b013377",
+    "url": "/pictures/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "49f885cb32de54f46c66",
+    "url": "/pictures/static/css/main.dbcda955.chunk.css"
+  },
+  {
+    "revision": "faef04adf4487b013377",
+    "url": "/pictures/static/js/2.a9a7b6db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pictures/static/js/2.a9a7b6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f885cb32de54f46c66",
+    "url": "/pictures/static/js/main.d268403a.chunk.js"
+  },
+  {
+    "revision": "bc2f8f310762781c91cb",
+    "url": "/pictures/static/js/runtime-main.58e9f121.js"
+  }
+]);
